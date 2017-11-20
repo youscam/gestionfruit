786304c3115bfc2e0e90d6b4df70ebb1b90d9fdf
kamal_azzouzi <youscam@outlook.com>
2017-11-20 22:54:43 +0000

    First Commit

---
+++ gestionfruit-ejb/src/main/java/ma/sylob/business/Single.java
@@ -0,0 +1,20 @@
+package ma.sylob.business;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class Single
+ */
+@Stateless
+@LocalBean
+public class Single {
+
+    /**
+     * Default constructor. 
+     */
+    public Single() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
